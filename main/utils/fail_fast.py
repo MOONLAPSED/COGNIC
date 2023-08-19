1b9840e0bede6e9e220b52424dfb654a0fa2d5ae
MOONLAPSED <137737122+MOONLAPSED@users.noreply.github.com>
2023-08-19 19:34:15 +0000

    YAML , house-cleaning

--- main/utils/fail_fast.py
+++
@@ -1,9 +0,0 @@
-class FailFastException(Exception):
-    pass
-
-def fail_fast(condition, message):
-    """
-    If the condition is False, raise an exception to fail fast.
-    """
-    if not condition:
-        raise FailFastException(message)
